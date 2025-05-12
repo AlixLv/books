526578bf96dabe5079c3b5cee472b55f9598641c
Alix <alix.leve@gmail.com>
2025-05-12 11:04:48 +0200

    initiate controllers folder

--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -4,8 +4,18 @@ from pydantic import BaseModel, EmailStr
 class UserSchema(BaseModel):
     name: str
     email: EmailStr
-    password: str
-    
+  
     class Config:
         # configuration Pydantic pour convertir objets SQLAlchemy en schémas Pydantic
         orm_mode = True
+
+
+class UserLogged(UserSchema):
+    password: str
+    class Config:
+        orm_mode = True    
+
+class UserId(UserSchema):
+    id: int
+    class Config:
+        orm_mode = True    
